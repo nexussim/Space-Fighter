a91b95cf7c007357f267f0f75b3920e0597036c4
Andrew <nexus_sim@hotmail.com>
2021-01-02 17:54:15 -0800

    Reworked logic.

--- app.js
+++ app.js
@@ -1,29 +1,107 @@
-let board = document.getElementById('board');
-let shipPositionX;
-let shipPositionY;
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext("2d");
+ctx.fillStyle = 'black';
+ctx.fillRect(0, 0, canvas.width, canvas.height)
 
-const ship = () => {
-    let spaceship = document.createElement('div');
-    spaceship.setAttribute('id', 'spaceship');
-    board.appendChild(spaceship);
-    shipPositionX = document.getElementById("spaceship").style.marginLeft = "350px";
-    shipPositionY = document.getElementById("spaceship").style.marginTop = "365px";
-}
+let shipPosition = { x: 350, y: 368 };
+let enemyShips = [];
+let laserShots = [];
+let shipPositionX = shipPosition.x;
+let shipPositionY = shipPosition.y;
 
-ship();
 
-const movement = (event) => {
-    if (event.keyCode === 39 && parseInt(shipPositionX) < 680) {
-        let spaceshipStyles = document.getElementById('spaceship')
-        let value = parseInt(shipPositionX) + 10;
-        spaceshipStyles.style.marginLeft = value.toString() + 'px';
-        shipPositionX = value.toString() + 'px';
+setInterval(function() {
+
+    draw()
+
+}, 0)
+
+const draw = () => {
+
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, canvas.width, canvas.height) 
+    spaceship();
+
+    /* ENEMIES */
+    
+    for (let i = 0; i < enemyShips.length; i++) {
+        ctx.fillStyle = enemyShips[i].color;
+        ctx.fillRect(enemyShips[i].x, enemyShips[i].y, 10, 20) 
+        if (enemyShips[i].y === canvas.height + 5) {
+            enemyShips.splice(i, 1);
+        }
     }
 
-    if (event.keyCode === 37 && parseInt(shipPositionX) > 0) {
-        let spaceshipStyles = document.getElementById('spaceship')
-        let value = parseInt(shipPositionX) - 10;
-        spaceshipStyles.style.marginLeft = value.toString() + 'px';
-        shipPositionX = value.toString() + 'px';
+    /* LASER CANON */
+
+    for (let j = 0; j < laserShots.length; j++) {
+        if (laserShots.length > 1) {
+            ctx.fillStyle = laserShots[j].color;
+            ctx.fillRect(laserShots[j].x + 8.5, laserShots[j].y, 2, 2) 
+            laserShots[j].y -= 5 ;
+        }
+        
     }
 }
+
+const spaceship = () => {
+
+    ctx.fillStyle = "green"
+    ctx.fillRect(shipPosition.x, shipPosition.y, 20, 30);
+
+}
+
+const laserCanon = () => {
+    laserShots.push({x: shipPosition.x, y: shipPosition.y, color: 'yellow'})
+}
+
+/* ENEMIES */
+
+setInterval(function() {
+
+    enemyShip()
+
+}, 3000)
+
+const enemyShip = () => {
+    let noRepeats = false;
+    let xCoor = randomCoor();
+    for (let i = 0; i < enemyShips.length; i++) {
+        if (xCoor === enemyShips[i].x) {
+            noRepeats = true;
+        }
+    }
+    if (!noRepeats && enemyShips.length < 10) {
+        enemyShips.push({x: xCoor, y: 5, color: 'red'});
+    }
+}
+
+setInterval(function() {
+    enemyShipMov();
+}, 500)
+
+const enemyShipMov = () => {
+    for (let i = 0; i < enemyShips.length; i++) {
+        enemyShips[i].y += 10;
+    }
+}
+
+const randomCoor = () => {
+
+    let num = Math.floor(Math.random() * Math.floor(680));
+    let decimal = num / 15;
+    return Math.round(decimal) * 15;
+
+}
+
+document.addEventListener('keydown', function(event) {
+
+    if (event.key === 'ArrowRight' && shipPosition.x !== canvas.width - 20) {
+        shipPosition.x += 10;
+    } else if (event.key === 'ArrowLeft' && shipPosition.x !== 0) {
+        shipPosition.x -= 10;
+    } else if (event.code === 'Space') {
+        laserCanon();
+    }
+})
+
