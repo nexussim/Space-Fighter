3a493b6c30e7a5163711c24d49dd5c5f3d910539
Andrew <nexus_sim@hotmail.com>
2021-01-03 08:57:37 -0800

    Laser cannon fire rate changes.

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ let shipPositionX = shipPosition.x;
 let shipPositionY = shipPosition.y;
 let input_state = {};
 let arrowKeyPressed;
+let laserId = 0;
 
 
 
@@ -39,17 +40,17 @@ function gameloop() {
         }
             
         if (input_state[" "]) {
-            laserCanon();
+            laserCannon();
         }
 
         for (let j = 0; j < laserShots.length; j++) {
-            if (laserShots.length > 1) {
+            if (laserShots.length > 1 && isMultipleOfThree(laserShots[j].id)) {
                 ctx.fillStyle = 'yellow';
                 ctx.fillRect(laserShots[j].x + 8.5, laserShots[j].y, 2, 2);
-                laserShots[j].y -= .5;
+                laserShots[j].y -= 7;
             }
         }
-        
+
         ctx.fillStyle = "green"
         ctx.fillRect(shipPosition.x, shipPosition.y, 20, 30);
 
@@ -85,8 +86,12 @@ const enemyShip = () => {
     }
 }
 
-const laserCanon = () => {
-    laserShots.push({x: shipPosition.x, y: shipPosition.y, color: 'yellow'})
+const laserCannon = () => {
+    laserShots.push({x: shipPosition.x, y: shipPosition.y, color: 'yellow', id: laserId})
+    laserId++;
+    if (laserShots.length > 57) {
+        laserShots.splice(0, 1);
+    }
 }
 
   
@@ -110,134 +115,11 @@ document.onkeyup = function(e) {
 
 init();
 
+const isMultipleOfThree = num => {
+    const div = parseInt(num / 7);
+  
+    return num === div * 7;
+  };
 
 
-//} else if (event.code === 'Space') {
-    //         laserCanon();
-    //     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// OLD CODE
-
-// let canvas = document.getElementById('canvas');
-// let ctx = canvas.getContext("2d");
-// ctx.fillStyle = 'black';
-// ctx.fillRect(0, 0, canvas.width, canvas.height)
-
-// let shipPosition = { x: 350, y: 368 };
-// let enemyShips = [];
-// let laserShots = [];
-// let shipPositionX = shipPosition.x;
-// let shipPositionY = shipPosition.y;
-
-
-// setInterval(function() {
-
-//     draw()
-
-// }, 0)
-
-// const draw = () => {
-
-//     ctx.fillStyle = 'black';
-//     ctx.fillRect(0, 0, canvas.width, canvas.height) 
-//     spaceship();
-
-//     /* ENEMIES */
-    
-//     for (let i = 0; i < enemyShips.length; i++) {
-//         ctx.fillStyle = enemyShips[i].color;
-//         ctx.fillRect(enemyShips[i].x, enemyShips[i].y, 10, 20) 
-//         if (enemyShips[i].y === canvas.height + 5) {
-//             enemyShips.splice(i, 1);
-//         }
-//     }
-
-//     /* LASER CANON */
-
-//     for (let j = 0; j < laserShots.length; j++) {
-//         if (laserShots.length > 1) {
-//             ctx.fillStyle = laserShots[j].color;
-//             ctx.fillRect(laserShots[j].x + 8.5, laserShots[j].y, 2, 2) 
-//             laserShots[j].y -= 5 ;
-//         }
-        
-//     }
-// }
-
-// const spaceship = () => {
-
-//     ctx.fillStyle = "green"
-//     ctx.fillRect(shipPosition.x, shipPosition.y, 20, 30);
-
-// }
-
-// const laserCanon = () => {
-//     laserShots.push({x: shipPosition.x, y: shipPosition.y, color: 'yellow'})
-// }
-
-// /* ENEMIES */
-
-// setInterval(function() {
-
-//     enemyShip()
-
-// }, 3000)
-
-// const enemyShip = () => {
-//     let noRepeats = false;
-//     let xCoor = randomCoor();
-//     for (let i = 0; i < enemyShips.length; i++) {
-//         if (xCoor === enemyShips[i].x) {
-//             noRepeats = true;
-//         }
-//     }
-//     if (!noRepeats && enemyShips.length < 10) {
-//         enemyShips.push({x: xCoor, y: 5, color: 'red'});
-//     }
-// }
-
-// setInterval(function() {
-//     enemyShipMov();
-// }, 500)
-
-// const enemyShipMov = () => {
-//     for (let i = 0; i < enemyShips.length; i++) {
-//         enemyShips[i].y += 10;
-//     }
-// }
-
-// const randomCoor = () => {
-
-//     let num = Math.floor(Math.random() * Math.floor(680));
-//     let decimal = num / 15;
-//     return Math.round(decimal) * 15;
-
-// }
-
-// document.addEventListener('keydown', function(event) {
-
-//     if (event.key === 'ArrowRight' && shipPosition.x !== canvas.width - 20) {
-//         shipPosition.x += 10;
-//     } else if (event.key === 'ArrowLeft' && shipPosition.x !== 0) {
-//         shipPosition.x -= 10;
-//     } else if (event.code === 'Space') {
-//         laserCanon();
-//     }
-// })
 
